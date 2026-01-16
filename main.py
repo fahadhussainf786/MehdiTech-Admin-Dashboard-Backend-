64013a4e5873c35e93923ce9300fcf626ff6392a
Fahad Hussain <fahadhussainf786@yahoo.com>
2026-01-16 14:37:03 +0500

    changes to cors

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
-    "https://admin-section-mehdi-tech.vercel.app/"
+    "https://admin-section-mehdi-tech.vercel.app"
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
